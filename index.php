d6558eb970ea23848387085d08a1f5be0ff88728
Rahul-Sagore <rahul.sagore@gmail.com>
2014-11-27 13:27:35 +0530

    Resolved the bug related to Bootstrap Modal window on index.php

--- index.php
+++ index.php
@@ -26,7 +26,6 @@
 </div>  <!--closing of outermost panel-->
 
 <div class="row alert alert-info">
-
     <div class="col-sm-1" style="margin-left:-10px;">
      
     </div>
@@ -46,30 +45,37 @@
             <div class="panel-heading">
                 <h2 class="panel-title text-center">Existing User</h2>
             </div>
-            <h2 align="center"><a href="#Modal" role="button" class="btn" data-toggle="modal">For Resume</a></h2>
-            
-        </div> 
+            <!-- Button trigger modal -->
+			<a  href="#" class="" data-toggle="modal" data-target="#myModal">
+			  <h2 align="center">For Resume</h2>
+			</a>
+
+			<!-- Modal -->
+			<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			  <div class="modal-dialog">
+				<div class="modal-content">
+				  <div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title" id="myModalLabel">Modal title</h4>
+				  </div>
+				  <div class="modal-body">
+					...
+				  </div>
+				  <div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				  </div>
+				</div><!-- ./modal-content -->
+			  </div>
+			</div><!-- ./modal-dialog -->
+        </div> <!-- ./modal -->
     </div>
 </div> 
 
 
-<!-- Modal -->
-<div id="Modal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h3 id="myModalLabel">Modal header</h3>
-  </div>
-  <div class="modal-body">
-    <p>One fine body…</p>
-  </div>
-  <div class="modal-footer">
-    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-    <button class="btn btn-primary">Save changes</button>
-  </div>
-</div>
 
 
-
+<script src="assests/js/jquery.min.js"></script>
 <script src="assests/js/bootstrap.min.js"></script>
 
 </body>
